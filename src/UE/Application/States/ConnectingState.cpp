9bd4bc67c3abeab76dfd744744c2ad8829b5e9f5
JakubKw19 <kkwiecie@interia.pl>
2025-04-13 18:29:18 +0200

    attach to bts made

--- src/UE/Application/States/ConnectingState.cpp
+++ src/UE/Application/States/ConnectingState.cpp
@@ -1,4 +1,6 @@
 #include "ConnectingState.hpp"
+#include "NotConnectedState.hpp"
+#include "ConnectedState.hpp"
 
 namespace ue
 {
@@ -6,6 +8,25 @@ namespace ue
 ConnectingState::ConnectingState(Context &context)
     : BaseState(context, "ConnectingState")
 {
+    logger.logDebug("entry");
+    context.user.showConnecting();
+}
+
+void ConnectingState::handleAttachAccept()
+{
+    context.timer.stopTimer();
+    context.setState<ConnectedState>();
+}
+
+void ConnectingState::handleAttachReject()
+{
+    context.timer.stopTimer();
+    context.setState<NotConnectedState>();
+}
+
+void ConnectingState::handleTimeout()
+{
+    context.setState<NotConnectedState>();
 }
 
 }
